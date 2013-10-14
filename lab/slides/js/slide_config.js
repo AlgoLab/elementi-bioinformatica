ce4fb859fc7a805e3409a659a267682d7e01df83
Yuri Pirola <yuri.pirola@gmail.com>
2013-10-15 00:24:37 +0200

    Add index of lab slides.

---
+++ lab/slides/js/slide_config.js
@@ -0,0 +1,12 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  }
+};
+
---
+++ lab/slides/js/slides.js
@@ -0,0 +1,3 @@
+require(['order!require-1.0.8.min', 'order!slide_config', 'order!modernizr.custom.45394', 'order!slide-controller',
+         'order!slide-deck'], function(someModule) {
+});
