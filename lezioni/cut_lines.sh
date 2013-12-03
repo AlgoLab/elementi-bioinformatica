6b2a1cd96c1f4f8c527ca7ab6d7e18e3103cefb7
Gianluca Della Vedova <gianluca@dellavedova.org>
2013-12-03 10:59:53 +0100

    better handling of subprojects

--- lezioni/cut_lines.sh
+++ lezioni/cut_lines.sh
@@ -6,8 +6,9 @@
 
 input_file="$1"
 output_dir="$2"
+test -d "$2" || mkdir -p "$2"
 for id in `grep '/*** start:'  $input_file | cut -d ' ' -f 3`
 do
     echo "$output_dir/$id"
-    awk "/start: $id/{a=1;next}/end: $id/{a=0}a" $input_file > "$output_dir/$id"
+    awk "/start: $id/{a=1;next}/\/\*\*\*end: /{a=0}a" $input_file > "$output_dir/$id"
 done
