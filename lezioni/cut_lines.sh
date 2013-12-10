bb68540e13fe9448d94683ad0d0ecb67d761d1eb
Gianluca Della Vedova <gianluca@dellavedova.org>
2013-12-10 14:20:19 +0100

    samtools

--- lezioni/cut_lines.sh
+++ lezioni/cut_lines.sh
@@ -10,5 +10,5 @@ test -d "$2" || mkdir -p "$2"
 for id in `grep '/*** start:'  $input_file | cut -d ' ' -f 3`
 do
     echo "$output_dir/$id"
-    awk "/start: $id/{a=1;next}/\/\*\*\*end: /{a=0}a" $input_file > "$output_dir/$id"
+    awk "/start: $id/{a=1;next}/***end: /{a=0}a" $input_file > "$output_dir/$id"
 done
